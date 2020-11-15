2c914c6aefc706a2949330a716ba64b4efe470e4
sky2145 <74121357+sky2145@users.noreply.github.com>
2020-11-15 16:24:47 +0900

    Initial commit

---
+++ Assets/unity-chan!/Unity-chan! Model/Art/Models/UnityChanController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanController : MonoBehaviour
+{
+    //アニメーションするためのコンポーネントを入れる
+    private Animator myAnimator;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        //Animatorコンポーネントを取得
+        this.myAnimator = GetComponent<Animator>();
+
+        //走るアニメーションを開始
+        this.myAnimator.SetFloat("Speed", 1);
+    }
+    void Update()
+    {
+
+    }
+}

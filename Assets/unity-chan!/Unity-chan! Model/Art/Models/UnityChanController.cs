22515ab76efe4260b2f0c85bccdc3c62b0d89bbe
sky2145 <74121357+sky2145@users.noreply.github.com>
2020-11-28 15:07:23 +0900

    ジャンプスクリプト変更

--- Assets/unity-chan!/Unity-chan! Model/Art/Models/UnityChanController.cs
+++ Assets/unity-chan!/Unity-chan! Model/Art/Models/UnityChanController.cs
@@ -5,19 +5,67 @@ public class UnityChanController : MonoBehaviour
 {
     //アニメーションするためのコンポーネントを入れる
     private Animator myAnimator;
-
+    //Unityちゃんを移動させるコンポーネントを入れる（追加）
+    private Rigidbody myRigidbody;
+    //前方向の速度（追加）
+    private float velocityZ = 16f;
+    //横方向の速度（追加）
+    private float velocityX = 10f;
+    //上方向の速度（追加）
+    private float velocityY = 10f;
+    //左右の移動できる範囲（追加）
+    private float movableRange = 3.4f;
     // Use this for initialization
     void Start()
     {
-
-        //Animatorコンポーネントを取得
+        //アニメータコンポーネントを取得
         this.myAnimator = GetComponent<Animator>();
 
         //走るアニメーションを開始
         this.myAnimator.SetFloat("Speed", 1);
+
+        //Rigidbodyコンポーネントを取得（追加）
+        this.myRigidbody = GetComponent<Rigidbody>();
     }
+
+    // Update is called once per frame
     void Update()
     {
+        //横方向の入力による速度（追加）
+        float inputVelocityX = 0;
+        //上方向の入力による速度（追加）
+        float inputVelocityY = 0;
+        //Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる（追加）
+        if (Input.GetKey(KeyCode.LeftArrow) && -this.movableRange< this.transform.position.x)
+        {
+            //左方向への速度を代入（追加）
+            inputVelocityX = -this.velocityX;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < this.movableRange)
+        {
+            //右方向への速度を代入（追加）
+            inputVelocityX = this.velocityX;
+        }
+        //ジャンプしていない時にスペースが押されたらジャンプする（追加）
+        if (Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f)
+        {
+            //ジャンプアニメを再生（追加）
+            this.myAnimator.SetBool("Jump", true);
+            //上方向への速度を代入（追加）
+            inputVelocityY = this.velocityY;
+        }
+        else
+        {
+            //現在のY軸の速度を代入（追加）
+            inputVelocityY = this.myRigidbody.velocity.y;
+        }
 
+        //Jumpステートの場合はJumpにfalseをセットする（追加）
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
+        }
+        //Unityちゃんに速度を与える（追加）
+        this.myRigidbody.velocity = new Vector3(inputVelocityX, inputVelocityY, velocityZ);
     }
 }

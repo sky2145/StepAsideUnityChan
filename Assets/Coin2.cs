d7d310d975af3c057a407c779e1b6fd00d1920cf
sky2145 <74121357+sky2145@users.noreply.github.com>
2021-01-10 19:38:16 +0900

    変更

---
+++ Assets/Coin2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin2 : MonoBehaviour
+{
+    private GameObject unitychan;
+    void Start()
+    {
+        //シーン中のunitychanオブジェクトを取得
+        this.unitychan = GameObject.Find("unitychan");
+    }
+    void Update()
+    {
+        if (this.transform.position.z < unitychan.transform.position.z)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
